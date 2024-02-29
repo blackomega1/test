28fec38d09a7e42668dc61907ecf41db24cb5299
blackomega1 <151652638+blackomega1@users.noreply.github.com>
2024-02-29 17:07:22 +0300

    Update server.ts

--- server.ts
+++ server.ts
@@ -3,7 +3,7 @@ import { serve } from 'https://deno.land/std/http/server.ts';
 const server = serve({ port: 8000 });
 console.log('Server running on http://localhost:8000');
 
-for await (const req of server) {
+for (const req of server) {
   const { method, url } = req;
 
   if (method === 'GET' && url === '/') {
